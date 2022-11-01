763b77d68b4e2af47c4c2a6321d12ec5ec837249
Sage-Park <sagepark94@gmail.com>
2022-11-01 22:35:52 +0900

    create Spring Project

---
+++ src/main/java/com/sage/tddplaner/TddPlanerApplication.java
@@ -0,0 +1,13 @@
+package com.sage.tddplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddPlanerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddPlanerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sage/tddplaner/TddPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sage.tddplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
