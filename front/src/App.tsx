22bb01bbd286a39a6e3a80eebc932c324067983b
Sage-Park <sagepark94@gmail.com>
2022-11-12 18:13:20 +0900

    refactoring

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,9 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="w-24 h-24 rounded-full">
-        App
-      </div>
+      <div className="w-24 h-24 rounded-full">App</div>
     </div>
   );
 }
